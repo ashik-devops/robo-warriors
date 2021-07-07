627e54168fa7877e7a7e039fcbec3c6b0dfa84e8
ashik-devops <mahmud.devops@gmail.com>
2021-07-07 18:42:50 +0200

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Card= () =>{
+    return(
+      <div>
+          <img src=''/>
+          <div>
+              <h2>Ashik Mahmud</h2>
+              <p>ashik@clearxaligner.com</p>
+          </div>
+      </div>
+    );
+}
