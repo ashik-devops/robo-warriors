4fcd0d1f83447d4e1ed98cf52d082c4f46afbec6
ashik-devops <mahmud.devops@gmail.com>
2021-07-15 01:30:48 +0200

    added some classes and responses

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@ import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import Scroll from "./Scroll";
-import "./App.css"
-import {robots} from "./robots";
+import "./App.css";
 class App extends Component {
     constructor(){
         super();
         this.state={
-            robots:robots,
+            robots:[],
             searchField:""
         }
     }
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response=> response.json()).then(users=>  this.setState({robots: users}))
+    }
+    
     onSearchChange=(event)=>{
         this.setState( {searchField: event.target.value})
         console.log("event", event.target.value);
     }
     render(){
         const filteredRobots= this.state.robots.filter(robots=>{
-            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())})
+        if(filteredRobots.length===0){
+                return <h1 className="tc" >Loading</h1>
         }
-
-        )
-        return(
-            <div className="tc">
-                <h1 className="f2" >Robo Friends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Scroll>
-                    <CardList robots={filteredRobots}/>
-                </Scroll>
-            </div>
-        )
+        else{
+            return(
+                <div className="tc">
+                    <h1 className="f2" >Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }
     } 
 }
 export default App;
